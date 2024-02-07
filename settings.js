c7d0ab156d0bc23026063392f24d90fcc39dfb27
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-02-07 18:06:29 +0700

    settings.js

--- settings.js
+++ settings.js
@@ -26,18 +26,18 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "EzaBot-MD" //namabot kalian
-global.ownername= "⛁ RezaOfc ×፝֟͜×" //nama kalian
+global.botname = "Kιɳα Bσƚ" //namabot kalian
+global.ownername= "Iky𝖔𝖋𝖋𝖎𝖈𝖎𝖆𝖑ཽ" //nama kalian
 global.myweb ="https://instagram.com/gamer.s_official" //bebas asal jan hapus
 global.youtube = "https://youtube.com/c/Mr18" //bebas asal jan hapus
 global.myweb2 = "https://instagram.com/gamer.s_official" //bebas
 global.email = "nb.boruto.official@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['6289606790112'] //premium user
-global.owner = ["6289606790112"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "6289606790112" // nomor wa kalian
-global.ownernomerr = "+6289606790112" //nmr wa kalian
+global.premium = ['6285794908894'] //premium user
+global.owner = ["6285794908894"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6285794908894" // nomor wa kalian
+global.ownernomerr = "+6285794908894" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
